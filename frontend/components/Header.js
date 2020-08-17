61bda3f2364f05291b2db6affec02cd4125ae9b3
cokoyoh <charlesokoyoh@gmail.com>
2020-08-17 22:17:07 +0300

    Adding app components for the home page

---
+++ frontend/components/Header.js
@@ -0,0 +1,18 @@
+import Nav from './Nav';
+
+const Header = () => (
+    <div>
+        <div class="bar">
+            <a href="#">Sick Fits</a>
+            <Nav/>
+        </div>
+
+        <div class="sub-bar">
+            <p>Search</p>
+        </div>   
+
+        <div>Cart</div>
+    </div>
+) 
+
+export default Header;
