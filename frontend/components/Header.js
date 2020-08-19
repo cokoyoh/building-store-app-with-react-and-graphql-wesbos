feb3bfd7c4b061d3c78031b9aa9d92ff1da41b83
cokoyoh <charlesokoyoh@gmail.com>
2020-08-19 22:20:46 +0300

    Visualizng route changes

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,6 +1,18 @@
 import Nav from './Nav';
 import Link from 'next/link';
 import styled from 'styled-components';
+import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = () => {
+    NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+    NProgress.done();
+};
+Router.onRouteChangeError = () => {
+    NProgress.done();
+}; 
 
 const Logo = styled.h1`
     font-size: 4rem;
@@ -43,16 +55,16 @@ const StyledHeader = styled.header`
 
 const Header = () => (
     <StyledHeader>
-        <div class="bar">
+        <div className="bar">
             <Logo>
-                <Link href="/">
+                <Link href="/">  
                     <a>Sick Fits</a>
                 </Link>
             </Logo>
             <Nav />
         </div>
  
-        <div class="sub-bar">
+        <div className="sub-bar">
             <p>Search</p>
         </div>   
 
